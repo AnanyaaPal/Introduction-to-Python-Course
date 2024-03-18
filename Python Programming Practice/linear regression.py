c9dd24154798fb6da86799aa711508e64e0c4e85
AnanyaaPal <149021499+AnanyaaPal@users.noreply.github.com>
2024-03-18 17:34:39 +0100

    disabling smart send

--- Python Programming Practice/linear regression.py
+++ Python Programming Practice/linear regression.py
@@ -8,3 +8,7 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import metrics
+
+#import the dataset
+df=pd.read_csv('C:\Users\Ananya Pal\Documents\TUD\1st semester\Introduction to Python\Lessons\Introduction-to-Python-Course\Python Programming Practice\HousingPrices.csv')
+
