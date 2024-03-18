1efd65cfa0108805ecaea9400e54e7302b168cf6
AnanyaaPal <149021499+AnanyaaPal@users.noreply.github.com>
2024-03-18 17:30:10 +0100

    import relevant libraries

---
+++ Python Programming Practice/linear regression.py
@@ -0,0 +1,10 @@
+#importing necessary libraries
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn import metrics
